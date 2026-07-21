b1a6cf7c5730b5547805a5b17ca754b4710d2f4b
kotlink <vovazimov23@gmail.com>
2026-07-21 22:22:58 +0300

    Запустил приложение с миграцией

--- Note/Program.cs
+++ Note/Program.cs
@@ -1,15 +1,25 @@
 using Microsoft.EntityFrameworkCore;
 using Note.Data;
+using Note.Endpoints;
+using Note.Servises.NoteServises;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddOpenApi();
 
+builder.Services.AddScoped<INoteServise, NoteServise>();
+
 builder.Services.AddDbContext<NoteDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<NoteDbContext>();
+    dbContext.Database.Migrate();
+}
 
 if (app.Environment.IsDevelopment())
 {
@@ -25,6 +35,8 @@ if (app.Environment.IsDevelopment())
 
 
 app.UseHttpsRedirection();
+
+app.MapNoteEndpoints();
 
 
 app.Run();
