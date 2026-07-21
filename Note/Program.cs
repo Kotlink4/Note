7c4c2b73f5283625b54d5c7286341d98ebb58861
kotlink <vovazimov23@gmail.com>
2026-07-21 20:48:15 +0300

    добавил свагер

--- Note/Program.cs
+++ Note/Program.cs
@@ -8,7 +8,15 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.DocumentTitle = "Note API";
+        options.SwaggerEndpoint("/openapi/v1.json", "Note V1");
+    });
 }
+
+
 
 app.UseHttpsRedirection();
 
